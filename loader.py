c0ad4cea5af856df448ab76cac45004018c1dbdc
Lil Lord <frududewa760@gmail.com>
2022-02-17 15:06:49 +0200

    'MySaveBot'

--- loader.py
+++ loader.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
 import configparser
+import os
 
 config = configparser.ConfigParser()
 config.read(r'data/config.ini')
@@ -12,6 +13,6 @@ bot = Bot(token=config['DEFAULT']['bot_token'],
           parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-engine = create_engine(f"sqlite:///data/main.db", echo=False)
+engine = create_engine("postgresql://scjmytdnbachvn:f13b47c4a417d436a691117290c7e07112681f7782c45e8511b6d4982267876c@ec2-52-209-246-87.eu-west-1.compute.amazonaws.com:5432/dpjikqddkqs7f", echo=False)
 base = declarative_base()
 session = sessionmaker(bind=engine)()
